509a7d0847c69979db6252baed7292f0141dc419
Anne Culpepper <anne.culpepper@gmail.com>
2020-03-28 18:33:02 -0500

    created a new module for calcHistory and connected it

---
+++ server/calcHistory.js
@@ -0,0 +1,16 @@
+module.exports = calcHistory = [
+  {
+    firstNumber: "",
+    operand: "",
+    secondNumber: "",
+    equals: "=",
+    answer: ""
+  },
+  {
+    firstNumber: "1",
+    operand: "+",
+    secondNumber: "1",
+    equals: "=",
+    answer: "2"
+  }
+];
--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const calcHistory = require("./calcHistory");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get("/calcHistory", (req, res) => {
+  res.send(calcHistory);
+  console.log(calcHistory);
+});
 
 app.listen(PORT, () => {
   console.log(`Hey Anne, your server is listening on PORT: ${PORT}`);
