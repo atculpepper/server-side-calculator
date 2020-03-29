cce70495ac6428dce341d296e925e526f487ef65
Anne Culpepper <anne.culpepper@gmail.com>
2020-03-29 15:52:06 -0500

    ajax promise on jquery file, render function appending calcHistory.js to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,36 @@ let newCalculation = {};
 function init() {
   console.log("DOM is connected");
   $(".submit-equals-btn").on("click", submitInputForms);
+
+  getCalculations();
+}
+
+function getCalculations() {
+  //AJAX request to server for calcHistory
+  $.ajax({
+    method: "GET",
+    url: "/calcHistory"
+  })
+    .then(response => {
+      render(response);
+    })
+    .catch(err => {
+      console.log("error");
+    });
+}
+
+//render calcHistory to DOM
+
+function render(calcHistory) {
+  console.log("Render calcHistory");
+  $(".div-calc-history").empty();
+  for (let i = 0; i < calcHistory.length; i++) {
+    const newCalculation = calcHistory[i];
+
+    $(".div-calc-history").append(`
+      <li>${newCalculation.firstNumber} ${newCalculation.operand} ${newCalculation.secondNumber} = ${newCalculation.answer}</li>
+    `);
+  }
 }
 
 function submitInputForms(event) {
