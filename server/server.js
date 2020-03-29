374868cbf86ef0b259c2d2ac7a800ba89171a8ba
Anne Culpepper <anne.culpepper@gmail.com>
2020-03-28 19:32:53 -0500

    get is working, post not yet working

--- server/server.js
+++ server/server.js
@@ -8,9 +8,19 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static("server/public"));
+
 app.get("/calcHistory", (req, res) => {
   res.send(calcHistory);
   console.log(calcHistory);
+});
+
+app.post("/calcHistory", (req, res) => {
+  const newCalculation = req.body;
+
+  calcHistory.push(newCalculation);
+
+  res.sendStatus(201);
 });
 
 app.listen(PORT, () => {
